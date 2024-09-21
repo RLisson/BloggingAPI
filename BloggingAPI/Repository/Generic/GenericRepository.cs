cf9e9b6b67929d5ca3ef79c81335127e5def7969
Lucas Risson <lucasrisson@gmail.com>
2024-09-21 11:22:27 -0300

    Created Generic Repository

---
+++ BloggingAPI/Repository/Generic/GenericRepository.cs
@@ -0,0 +1,92 @@
+﻿using BloggingAPI.Model.Base;
+using BloggingAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloggingAPI.Repository.Generic
+{
+    public class GenericRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        private MySQLContext _context;
+        private DbSet<T> dataset;
+
+        public GenericRepository(MySQLContext context)
+        {
+            _context = context;
+            dataset = _context.Set<T>();
+        }
+
+        public T Create(T item)
+        {
+            try
+            {
+                dataset.Add(item);
+                _context.SaveChanges();
+                return item;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var result = dataset.SingleOrDefault(p => p.Id == id);
+            if (result != null)
+            {
+                try
+                {
+                    dataset.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred");
+                    Console.WriteLine(ex.Message);
+                    throw;
+                }
+            }
+        }
+
+        public List<T> FindAll()
+        {
+            return dataset.ToList();
+        }
+
+        public T FindByID(int id)
+        {
+            return dataset.SingleOrDefault(p => p.Id == id);
+        }
+
+        public T Update(T item)
+        {
+            if (!Exists(item.Id)) return null;
+
+            var result = dataset.SingleOrDefault(p => p.Id == item.Id);
+
+            if (result != null)
+            {
+                try
+                {
+                    dataset.Entry(result).CurrentValues.SetValues(item);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred");
+                    Console.WriteLine(ex.Message);
+                    throw;
+                }
+            }
+
+            return item;
+        }
+
+        private bool Exists(int id)
+        {
+            return dataset.Any(p => p.Id == id);
+        }
+    }
+}
---
+++ BloggingAPI/Repository/Generic/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using BloggingAPI.Model.Base;
+
+namespace BloggingAPI.Repository.Generic
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T Create(T item);
+        T FindByID(int id);
+        List<T> FindAll();
+        T Update(T item);
+        void Delete(int id);
+    }
+}
